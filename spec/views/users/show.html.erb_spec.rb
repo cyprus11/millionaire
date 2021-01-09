60eb164c7f809d40dbd58445854f5959d017103a
Alexey <alexx10@mail.ru>
2021-01-09 17:56:41 +0300

    fix test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,45 +3,45 @@ require 'rails_helper'
 RSpec.describe 'users/show', type: :view do
   let(:current_user) { create(:user, name: 'Алексий', balance: 1000) }
 
-  before do
-    assign(:user, current_user)
-    assign(:games, [build_stubbed(:game)])
-
-    render
-  end
-
   context 'when users see his profile' do
+    before do
+      assign(:user, current_user)
+      assign(:games, [build_stubbed(:game)])
+
+      sign_in current_user
+      stub_template "users/_game.html.erb" => "User game"
+
+      render
+    end
+
     it 'render users name' do
       expect(rendered).to match('Алексий')
     end
 
     it 'render change password button in users profile for profile owner' do
-      sign_in current_user
-
-      render
-
       expect(rendered).to match('Сменить имя и пароль')
     end
 
     it 'renders game partial' do
-      stub_template "users/_game.html.erb" => "User game"
-
-      render
-
       expect(rendered).to have_content "User game"
     end
   end
 
   context 'when user check other profile' do
+    before do
+      assign(:user, current_user)
+      assign(:games, [build_stubbed(:game)])
+
+      stub_template "users/_game.html.erb" => "User game"
+
+      render
+    end
+
     it 'does not render change password button if users are not in his profile' do
       expect(rendered).not_to match('Сменить имя и пароль')
     end
 
     it 'renders game partial' do
-      stub_template "users/_game.html.erb" => "User game"
-
-      render
-
       expect(rendered).to have_content "User game"
     end
   end
