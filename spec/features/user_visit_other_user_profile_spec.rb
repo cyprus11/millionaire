d099412ec740abfa811af2f9da5ebe1cfb58e3b8
Alexey <alexx10@mail.ru>
2021-01-09 13:30:37 +0300

    fix feature test

--- spec/features/user_visit_other_user_profile_spec.rb
+++ spec/features/user_visit_other_user_profile_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'USER visit other user profile', typre: :feature do
-  let(:games) do
-    (1..2).to_a.map do |i|
-      create(:game,
-             prize: i * 100)
-    end
-  end
+  let(:games) { create_list(:game, 2, prize: 100) }
 
   let(:user) { create(:user) }
   let(:other_user) { create(:user, name: 'Василий', games: games) }
@@ -21,6 +16,6 @@ RSpec.feature 'USER visit other user profile', typre: :feature do
     expect(page).to have_current_path "/users/#{other_user.id}"
     expect(page).not_to have_content "Сменить имя и пароль"
     expect(page).to have_content I18n.l(Time.now, format: :short)
-    expect(page).to have_content i * 2
+    expect(page).to have_content 100
   end
 end
